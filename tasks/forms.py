3df1032470a64154124a0805f6c903d89b2f9962
Amaia <amaia.berrocal@gmail.com>
2020-01-09 20:00:49 +0100

    Estilo con skeleton y formulario con wtf

---
+++ tasks/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.widgets import TextArea
+from datetime import date
+
+def mayor_que_hoy(form, field):
+    hoy = date.today()
+    if field.data < hoy:
+        raise ValidationError('La fecha debe ser igual o posterior a hoy')
+
+class TaskForm(FlaskForm):
+    title = StringField('Título', validators=[DataRequired(), Length(min=3, max=15, message='La longitud ha de estar entre 3 y 15 caracteres.')])
+    description = StringField('Descripción', widget=TextArea())
+    date = DateField('Fecha', validators=[DataRequired(), mayor_que_hoy])
+
+    submit = SubmitField('Añadir tarea')
--- tasks/routes.py
+++ tasks/routes.py
@@ -1,11 +1,15 @@
 from tasks import app
 from flask import render_template, request, url_for, redirect
+from tasks.forms import TaskForm
+
 import csv
+
+DATOS = './data/tareas.dat'
 
 @app.route("/")
 def index():
-    fTareas = open('./data/tareas.dat', 'r')
-    csvreader = csv.reader(fTareas, delimiter=',')
+    fTareas = open(DATOS, 'r')
+    csvreader = csv.reader(fTareas, delimiter=',', quotechar= '"')
 
     datos = []
     for linea in csvreader:
@@ -16,21 +20,26 @@ def index():
     
 @app.route("/newtask", methods=['GET', 'POST'])
 def newtask():
+    form = TaskForm(request.form)
+
     if request.method == 'GET':
-        return render_template('task.html')
+        return render_template('task.html', form = form)
     
-    if request.method == 'POST': 
-        fdatos = open('./data/tareas.dat', 'a')
+    if form.validate():
+        fdatos = open(DATOS, 'a')
         csvwriter = csv.writer(fdatos, delimiter=',', quotechar= '"')
 
         title = request.values.get('title')
-        desc = request.values.get('desc')
+        desc = request.values.get('description')
         date = request.values.get('date')
 
         csvwriter.writerow([title, desc, date])
 
         fdatos.close()
         return redirect(url_for('index'))
+    else:
+        return render_template("task.html", form=form)
     
+
     
    
