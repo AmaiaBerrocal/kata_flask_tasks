25bc9d55457e7e41154fd542af150e8ee1e1b1bb
Amaia <amaia.berrocal@gmail.com>
2020-01-16 18:19:19 +0100

    ultimos arreglos

--- config_template.py
+++ config_template.py
@@ -1,1 +1,2 @@
 SECRET_KEY=b'<aquí metes tu clave>'
+DB_FILE='nombre del fichero sqlite3'
--- tasks/forms.py
+++ tasks/forms.py
@@ -2,25 +2,27 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Length, ValidationError, AnyOf
 from wtforms.widgets import TextArea
+
 from datetime import date
 
-def mayor_que_hoy(form, field):
+def greater_than_today(form, field):
     hoy = date.today()
     if field.data < hoy:
-        raise ValidationError('La fecha debe ser igual o posterior a hoy')
+        raise ValidationError('La fecha debe ser superior o igual a hoy')
+
 
 class TaskForm(FlaskForm):
-    title = StringField('Título', validators=[DataRequired(), Length(min=3, max=15, message='La longitud ha de estar entre 3 y 15 caracteres.')])
+    title = StringField('Título', validators=[DataRequired(), Length(min=3, max=15, message="La longitud ha de estar entre 3 y 15")])
     description = StringField('Descripción', widget=TextArea())
-    date = DateField('Fecha', validators=[DataRequired(), mayor_que_hoy])
+    fx = DateField('Fecha', validators=[DataRequired(), greater_than_today])
 
-    submit = SubmitField('Añadir tarea')
+    submit = SubmitField('Enviar')
 
-class ProcessTaskForm(FlaskForm):
+class ProccesTaskForm(FlaskForm):
     ix = HiddenField('ix', validators=[DataRequired()])
     btn = HiddenField('btn', validators=[DataRequired(), AnyOf(['M', 'B'])])
-    title = StringField('Título', validators=[DataRequired(), Length(min=3, max=15, message='La longitud ha de estar entre 3 y 15 caracteres.')])
+    title = StringField('Título', validators=[DataRequired(), Length(min=3, max=15, message="La longitud ha de estar entre 3 y 15")])
     description = StringField('Descripción', widget=TextArea())
-    date = DateField('Fecha', validators=[DataRequired(), mayor_que_hoy])
+    fx = DateField('Fecha', validators=[DataRequired()])
 
     submit = SubmitField('Aceptar')
